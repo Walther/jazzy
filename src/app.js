d1afc302208b480ef0b75498f8148ffe4f5b8e8d
Veeti Haapsamo <veeti.haapsamo@gmail.com>
2017-10-20 02:23:51 +0300

    Play back root notes of chord progression

--- src/app.js
+++ src/app.js
@@ -1,5 +1,7 @@
 const tonal = require('tonal');
 const fp = require('lodash/fp');
+const Tone = require('tone');
+var synth = new Tone.Synth().toMaster();
 
 const getScale = () => {
     let root = _.sample(tonal.Note.names());
@@ -17,6 +19,10 @@ const getChords = scale => {
         {
             degree: 5,
             type: '7'
+        },
+        {
+            degree: 1,
+            type: 'maj7'
         },
         {
             degree: 1,
@@ -50,3 +56,23 @@ let jazz = {
 };
 
 writeSheet(jazz);
+let toneNotes = chords.map(chord => {
+    let root = tonal.Chord.notes(chord)[0];
+    return root + '3';
+    // TODO: saner handling.
+    // Tone requires absolute note, and doesn't tolerate double sharps
+});
+
+console.log('ToneNotes: ' + toneNotes);
+
+toneNotes.map((toneNote, index) => {
+    Tone.Transport.scheduleRepeat(
+        function(time) {
+            synth.triggerAttackRelease(toneNote, '1n', time);
+        },
+        '4m',
+        index + 'm'
+    );
+});
+
+Tone.Transport.start();
