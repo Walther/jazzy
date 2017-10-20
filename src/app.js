d8350f323436e6447d685096ed47b15613439431
Veeti Haapsamo <veeti.haapsamo@gmail.com>
2017-10-20 17:31:44 +0300

    Switch libraries: tonal -> teoria. Introduce playSheet.

--- src/app.js
+++ src/app.js
@@ -1,17 +1,17 @@
-const tonal = require('tonal');
+const teoria = require('teoria');
 const fp = require('lodash/fp');
 const Tone = require('tone');
 var synth = new Tone.Synth().toMaster();
 var polySynth = new Tone.PolySynth(4, Tone.Synth).toMaster();
 
-const getScale = () => {
-    let root = _.sample(tonal.Note.names(' b'));
-    let scaleType = 'major';
-    let scale = root + ' ' + scaleType;
-    return scale;
-};
+const getRoot = () =>
+    // TODO: sane root options
+    _.sample(['C', 'Db', 'D', 'Eb', 'E', 'F', 'Gb', 'G', 'Ab', 'A', 'Bb', 'B']);
+
+const getScale = (root, scaleType) => teoria.note(root).scale(scaleType);
 
 const getChords = scale => {
+    //TODO: this assumes major scale
     let progression = [
         {
             degree: 2,
@@ -30,50 +30,53 @@ const getChords = scale => {
             type: 'maj7'
         }
     ];
-    let notes = tonal.Scale.notes(scale);
-    let chords = progression.map(
-        ({ degree, type }) => notes[degree - 1] + type
-    );
+    let notes = scale.notes();
+    let chords = progression.map(({ degree, type }) => {
+        return teoria.chord(notes[degree - 1], type);
+    });
     return chords;
 };
 
 const writeSheet = data => {
-    let prettyPrint = 'Scale: ' + data.scale;
-    prettyPrint += '<br> Chords: ' + data.chords.join(', ');
+    let prettyPrint = '';
+    prettyPrint += 'Key: ' + data.key;
+    prettyPrint += '<br>Scale: ' + data.scale;
+    prettyPrint += '<br>Chords: ' + data.chords.join(', ');
 
     document.getElementById('jazz').innerHTML = prettyPrint;
+};
+
+const playSheet = data => {
+    data.chords.map((chord, index) => {
+        let notes = chord.notes().map(note => note.toString());
+        Tone.Transport.scheduleRepeat(
+            time => {
+                polySynth.triggerAttackRelease(notes, '1n', time); // Repeat a 1n = whole note long chord
+            },
+            '4m', // every 4 measures
+            index + 'm' // at offset index measures
+        );
+    });
+
+    Tone.Transport.start();
 };
 
 /* --- --- --- */
 
 console.log('Hello, Jazz!');
 
-let scale = getScale();
+let root = getRoot();
+let absoluteRoot = root + '3';
+let scaleType = 'major';
+let key = root + ' ' + scaleType;
+let scale = getScale(absoluteRoot, scaleType);
 let chords = getChords(scale);
 
 let jazz = {
-    scale,
+    key,
+    scale: _.startCase(scale.simple()),
     chords
 };
 
 writeSheet(jazz);
-let toneChords = chords.map(chord => {
-    // Tone.js requires absolute note, i.e. octave number in addition to the note name
-    // Sadly some chords sound bad because of this naive implemenation, when a maj7 is played as a min2.
-    // TODO: correct implementation of absolute notes!
-    return tonal.Chord.notes(chord).map(note => note + '3');
-});
-
-console.log('ToneChords: ' + JSON.stringify(toneChords));
-
-toneChords.map((toneNotes, index) => {
-    Tone.Transport.scheduleRepeat(
-        time => {
-            polySynth.triggerAttackRelease(toneNotes, '1n', time); // Repeat a 1n = whole note long chord
-        },
-        '4m', // every 4 measures
-        index + 'm' // at offset index measures
-    );
-});
-
-Tone.Transport.start();
+playSheet(jazz);
